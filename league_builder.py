758913931c8b80471867d61f6cd82ba66dccd761
Trace Harris <traceh@whiletrace.com>
2018-07-02 19:56:13 -0700

    completed the initial script plan and Suedo code next step to create the classes

---
+++ league_builder.py
@@ -0,0 +1,32 @@
+
+import csv
+# Optional
+    # create classes and objects
+    # player class 
+        # player attributes
+        # name
+        # age
+        # experience
+        # gardians
+    # team class
+        # will have a name 
+        # will contain players
+        # each team will have equal number of players
+        # each team will have equal number of experienced players
+    # Cordinator class 
+        # cordinator will create players from the soccer_players csv
+            # read data from soccer_players csv
+            # convert data to a usable format I believe a set of dictionaries
+        # cordinator will create teams
+            #create three teams Raptors Dragons Sharks I believe the these too can be a set
+            # cordinator will distribute players to teams
+            # equal number of 
+
+
+
+
+# Each team has the same number of players and the same number of experienced players 
+# Create a text file that has each team name with the players listed underneath the team name
+# the players printed should be formatted such as name, age height, experienced,Gaurdians
+if __name__ == '__main__':
+    main()
