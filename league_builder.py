bb72599fceaef58dc1f0ea6ebf23ec1060948387
Trace Harris <traceh@whiletrace.com>
2018-07-09 23:07:42 -0700

    So built basic class structures set method on Cordinator class to open, read, and convert csv file to ordered dict

--- league_builder.py
+++ league_builder.py
@@ -8,15 +8,44 @@ import csv
         # age
         # experience
         # gardians
-    # team class
-        # will have a name 
-        # will contain players
-        # each team will have equal number of players
-        # each team will have equal number of experienced players
-    # Cordinator class 
-        # cordinator will create players from the soccer_players csv
-            # read data from soccer_players csv
-            # convert data to a usable format I believe a set of dictionaries
+
+# player class
+class player(object):
+    """docstring for player"""
+    def __init__(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+            pass
+
+# team class
+class team(object):
+    """docstring for team"""
+    def __init__(self, arg):
+        super(team, self).__init__()
+        self.arg = arg
+        pass
+    # will have a name
+    # will contain players
+    # each team will have equal number of players
+    # each team will have equal number of experienced players
+
+#coordinator class
+class cordinator(object):
+    """docstring for cordinator"""
+    def __init__(self, arg):
+        super(cordinator, self).__init__()
+        self.arg = arg
+        
+     # cordinator will create players from the soccer_players csv
+     def playercreate(self):
+        # read data from soccer_players csv
+        # convert data to a usable format I believe a set of dictionaries
+        with open('soccer_players.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                print(row['first_name'], row['last_name'])
+
+
         # cordinator will create teams
             #create three teams Raptors Dragons Sharks I believe the these too can be a set
             # cordinator will distribute players to teams
