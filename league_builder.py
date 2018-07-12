264a3e0230ee44a0b9e7be8e1628eca02f40a1dc
Trace Harris <traceh@whiletrace.com>
2018-07-11 20:24:11 -0700

    player create method for cordinator class working next step team build and team sort

--- league_builder.py
+++ league_builder.py
@@ -10,7 +10,7 @@ import csv
         # gardians
 
 # player class
-class player(object):
+class Player(object):
     """docstring for player"""
     def __init__(self, **kwargs):
         for key, value in kwargs.items():
@@ -30,32 +30,31 @@ class team(object):
     # each team will have equal number of experienced players
 
 #coordinator class
-class cordinator(object):
+class Cordinator():
     """docstring for cordinator"""
-    def __init__(self, arg):
-        super(cordinator, self).__init__()
-        self.arg = arg
-        
+    
      # cordinator will create players from the soccer_players csv
-     def playercreate(self):
+    def playercreate(self):
         # read data from soccer_players csv
-        # convert data to a usable format I believe a set of dictionaries
-        with open('soccer_players.csv', newline='') as csvfile:
+        # convert data to a usable format ordered dict
+        # create player class instances
+        # for check access player.Name 
+
+        with open('soccer_players.csv', newline = '') as csvfile:
             reader = csv.DictReader(csvfile)
-            for row in reader:
-                print(row['first_name'], row['last_name'])
-
-
-        # cordinator will create teams
+            players = [Player(**row) for row in reader]
+            for player in players:
+                print(player.Name)
+                
+        pass
+            # cordinator will create teams
             #create three teams Raptors Dragons Sharks I believe the these too can be a set
             # cordinator will distribute players to teams
             # equal number of 
-
-
-
-
 # Each team has the same number of players and the same number of experienced players 
 # Create a text file that has each team name with the players listed underneath the team name
 # the players printed should be formatted such as name, age height, experienced,Gaurdians
 if __name__ == '__main__':
-    main()
+    
+ John = Cordinator()
+ John.playercreate()
